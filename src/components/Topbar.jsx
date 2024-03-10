174641366b3c5ba0f48e160b3f390cb36d0fcd8f
Ri4Chaard <yarik271202@gmail.com>
2024-03-10 22:07:00 +0200

    Burger menu added, some styles edited

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,19 +1,23 @@
 import React, { useContext, useEffect, useState } from "react";
 import logo from "../img/logo.png";
+import loginLogo from "../img/user.svg";
+import loginLogoInv from "../img/userInv.svg";
 import { NavButton } from "../components/UI/buttons/header_buttons/nav_button/NavButton";
 import { LoginButton } from "../components/UI/buttons/header_buttons/login_button/LoginButton";
 import { Link } from "react-router-dom";
 import { PageContext } from "./context";
 import { useWindowSize } from "./hooks/useWindowSize";
+import { Burger } from "./UI/burger/Burger";
 
 export const Topbar = () => {
     const [windowHeight, setWindowHeight] = useState(0);
     const { currentPage } = useContext(PageContext);
     const currentWindowSize = useWindowSize(window.innerWidth);
+    const [burger, setBurger] = useState(false);
 
     const calcBP = (width) => {
         if (width < 992 && width > 720) return 586;
-        else if (width <= 720 && width > 360) {
+        else if (width <= 720 && width >= 360) {
             return 845;
         } else return 748;
     };
@@ -27,6 +31,9 @@ export const Topbar = () => {
         window.addEventListener("scroll", setFixed);
         return () => window.removeEventListener("scroll", setFixed);
     });
+    useEffect(() => {
+        setBurger(false);
+    }, [currentPage]);
 
     return (
         <div
@@ -58,38 +65,71 @@ export const Topbar = () => {
                         : {}
                 }
             >
-                <div className="topbar__logo">
-                    <Link key="/menu" to="/menu">
-                        <img className="logo" src={logo} alt="logo" />
-                    </Link>
-                </div>
-                <nav className="topbar__menu">
-                    <Link key="/catalog" to="/catalog">
-                        <NavButton
-                            style={{
-                                fontWeight: "500",
-                                color:
-                                    currentPage == "/catalog" ? "#5C17CD" : "",
+                {currentWindowSize > 720 ? (
+                    <>
+                        <div className="topbar__logo">
+                            <Link key="/menu" to="/menu">
+                                <img className="logo" src={logo} alt="logo" />
+                            </Link>
+                        </div>
+                        <nav className="topbar__menu">
+                            <Link key="/catalog" to="/catalog">
+                                <NavButton
+                                    style={{
+                                        fontWeight: "500",
+                                        color:
+                                            currentPage == "/catalog"
+                                                ? "#5C17CD"
+                                                : "",
+                                    }}
+                                >
+                                    Ready solutions
+                                </NavButton>
+                            </Link>
+                            <NavButton>Tariffs</NavButton>
+                            <Link key="/news" to="/news">
+                                <NavButton
+                                    style={{
+                                        color:
+                                            currentPage == "/news"
+                                                ? "#5C17CD"
+                                                : "",
+                                    }}
+                                >
+                                    News
+                                </NavButton>
+                            </Link>
+                            <NavButton>Contacts</NavButton>
+                        </nav>
+                        <div className="topbar__login">
+                            <LoginButton>Login</LoginButton>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div
+                            className="topbar__burger "
+                            onClick={() => {
+                                setBurger(!burger);
+                                console.log(burger);
                             }}
                         >
-                            Ready solutions
-                        </NavButton>
-                    </Link>
-                    <NavButton>Tariffs</NavButton>
-                    <Link key="/news" to="/news">
-                        <NavButton
-                            style={{
-                                color: currentPage == "/news" ? "#5C17CD" : "",
-                            }}
-                        >
-                            News
-                        </NavButton>
-                    </Link>
-                    <NavButton>Contacts</NavButton>
-                </nav>
-                <div className="topbar__login">
-                    <LoginButton>Login</LoginButton>
-                </div>
+                            <span></span>
+                        </div>
+                        <div className="topbar__loginIcon">
+                            <img
+                                src={
+                                    windowHeight >= breakPoint ||
+                                    currentPage != "/menu"
+                                        ? loginLogoInv
+                                        : loginLogo
+                                }
+                                alt="login"
+                            />
+                        </div>
+                        <Burger visible={burger} setVisible={setBurger} />
+                    </>
+                )}
             </div>
         </div>
     );
